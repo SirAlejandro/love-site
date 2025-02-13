87be4854d552fdecb960a334e39029043c7f75ff
SirAlejandro <63714969+SirAlejandro@users.noreply.github.com>
2025-02-13 19:56:55 +0300

    Create main.jsx
    add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LoveSite from "./LoveSite.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LoveSite />
+  </React.StrictMode>
+);
