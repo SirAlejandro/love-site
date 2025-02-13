25cde315aa4266f5317a697b9f04e796f8b5b2e8
SirAlejandro <63714969+SirAlejandro@users.noreply.github.com>
2025-02-13 20:01:13 +0300

    2
    Переместил LoveSite.jsx в src

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LoveSite from "./LoveSite.jsx";
+import LoveSite from "./LoveSite"; // Без .jsx
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
